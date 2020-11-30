5b1a6a2b0e150d79a4334356a03e3046ce7ffd7d
Daniel Pourhadi <dan@pourhadi.com>
2020-11-30 17:31:22 -0600

    Initial Commit

---
+++ sitediffer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sitediffer
+//
+//  Created by Daniel Pourhadi on 11/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sitediffer/sitedifferApp.swift
@@ -0,0 +1,17 @@
+//
+//  sitedifferApp.swift
+//  sitediffer
+//
+//  Created by Daniel Pourhadi on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct sitedifferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
